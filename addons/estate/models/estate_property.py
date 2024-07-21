9756d1be0ce713f466b5f388e55b52f4615715a6
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-07-21 17:54:11 +0800

    chapter 14

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -42,6 +42,7 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
         default='new',
+        group_expand="_expand_groups",
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     salesperson_id = fields.Many2one("res.users", string="Salesman", index=True, default=lambda self: self.env.user)
@@ -65,6 +66,10 @@ class EstateProperty(models.Model):
             if not float_is_zero(record.selling_price, precision_digits=precision):
                 if float_compare(record.selling_price, 0.9 * record.expected_price, precision_digits=precision) < 0:
                     raise ValidationError("Selling price cannot be lower than 90% of the expected price.")
+
+    @api.model
+    def _expand_groups(self, states, domain, order):
+        return ['new', 'offer_received', 'offer_accepted', 'sold', 'canceled']
 
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
